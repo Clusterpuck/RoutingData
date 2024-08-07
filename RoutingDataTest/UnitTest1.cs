1f6bc689ca0132cdd9ba91cc773e1d7a19f514fc
amiramoriff <moriffamira@gmail.com>
2024-08-07 20:12:21 +0800

    - add tests for get customer

--- RoutingDataTest/UnitTest1.cs
+++ RoutingDataTest/UnitTest1.cs
@@ -11,6 +11,7 @@ namespace RoutingDataTest
 {
     public class UnitTest1
     {
+        // TEST POST CUSTOMER
         [Fact]
         public async Task Test_PostCustomer()
         {
@@ -25,6 +26,8 @@ namespace RoutingDataTest
 
             Assert.Single(context.Customers);
         }
+
+        // TEST GET CUSTOMERS
         [Fact]
         public async Task Test_GetCustomers()
         {
@@ -44,6 +47,40 @@ namespace RoutingDataTest
             var actionResult = Assert.IsType<ActionResult<IEnumerable<Customer>>>(result); // check that the result is of type ActionResult<IEnumerable<Customer>>
             var returnValue = Assert.IsType<List<Customer>>(actionResult.Value);  // check that the Value property of the ActionResult is of type List<Customer>
             Assert.Equal(2, returnValue.Count); // check that the count of the returned list is 2
+        }
+
+        // TEST GET CUSTOMER
+        [Fact]
+        public async Task Test1_GetCustomer()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            await using var context = new ApplicationDbContext(optionsBuilder.Options);
+            var customer = new Customer { Name = "Fishy", Phone = "134454355" };
+            context.Customers.Add(customer);
+            await context.SaveChangesAsync();
+
+            var customersController = new CustomersController(context);
+            var result = await customersController.GetCustomer(customer.Id);
+
+            var actionResult = Assert.IsType<ActionResult<Customer>>(result);
+            var returnValue = Assert.IsType<Customer>(actionResult.Value);
+            Assert.Equal(customer.Name, returnValue.Name);
+            Assert.Equal(customer.Phone, returnValue.Phone);
+        }
+
+        [Fact]
+        public async Task Test2_GetCustomer()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            await using var context = new ApplicationDbContext(optionsBuilder.Options);
+            var customersController = new CustomersController(context);
+            var result = await customersController.GetCustomer(1);
+
+            Assert.IsType<NotFoundResult>(result.Result);
         }
     }
 }
