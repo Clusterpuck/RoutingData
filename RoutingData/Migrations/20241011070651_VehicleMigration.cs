cdb6daa0677e8bc86f1143e82e54e72cfb7bdd9d
ameliefishr <ameliefisher03@gmail.com>
2024-10-12 10:23:36 +0800

    Migating db

---
+++ RoutingData/Migrations/20241011070651_VehicleMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoutingData.Migrations
+{
+    /// <inheritdoc />
+    public partial class VehicleMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Colour",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Make",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Colour",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Make",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Vehicles");
+        }
+    }
+}
